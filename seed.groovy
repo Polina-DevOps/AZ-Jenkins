b9e909d3dd2ac8521f126ea197d9c8f1b8a0479d
Nageswar Polina <polinadevops2021@gmail.com>
2021-08-22 16:29:36 +0800

    Seedjobs V1.2.5

--- seed.groovy
+++ seed.groovy
@@ -1,16 +1,26 @@
-freeStyleJob('example') {
-    logRotator(-1, 10)
-    jdk('Java 8')
-    scm {
-        github('jenkinsci/job-dsl-plugin', 'master')
-    }
-    triggers {
-        githubPush()
-    }
-    steps {
-        gradle('clean build')
-    }
-    publishers {
-        archiveArtifacts('job-dsl-plugin/build/libs/job-dsl.hpi')
+folder('CI-Pipelines') {
+    displayName('CI-Pipelines')
+    description('CI-Pipelines')
+}
+
+pipelineJob('CI-Pipelines/frontend') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/frontend')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/master')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
     }
 }
+
